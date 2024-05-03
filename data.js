b50ca9df09836ade2388bacf442bc05d7aac888a
Jia XU <xujia118@hotmail.com>
2024-05-02 20:27:55 -0700

    Added dynamic sample in server.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const tasks = {
+  abc123: {
+    _id: "abc123",
+    title: "Task 1",
+    description: "Description of Task 1",
+  },
+  def456: {
+    _id: "def456",
+    title: "Task 2",
+    description: "Description of Task 2",
+  },
+  ghi789: {
+    _id: "ghi789",
+    title: "Task 3",
+    description: "Description of Task 3",
+  },
+};
+
+module.exports = tasks;
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const tasks = require("./data");
+
+app.get("/tasks", (req, res) => {
+  res.send(`
+        <>Task List</
+        <ul>
+            ${Object.values(tasks)
+              .map(
+                (task) =>
+                  `<li><a href="/tasks/${task._id}">${task.title}</a> - ID: ${task.description}</li>`
+              )
+              .join("")}
+        </ul>
+    `);
+});
+
+app.get("/tasks/:taskId", (req, res) => {
+  const taskId = req.params.taskId;
+
+//   console.log(req.params);
+
+  // Find the task in the database by ID
+  const task = tasks[taskId];
+
+  if (!task) {
+    return res.status(404).send("Task not found");
+  }
+  // Render the task details page
+  res.send(`
+        <h1>Task Details</h1>
+        <p><strong>Title:</strong> ${task.title}</p>
+        <p><strong>Description:</strong> ${task.description}</p>
+        <p><strong>ID:</strong> ${task.id}</p>
+    `);
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
